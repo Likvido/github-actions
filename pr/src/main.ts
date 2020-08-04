97f92504a6fb122506566171883032e68a58042c
Alexander Boychenko <boichenko.alexander@gmail.com>
2020-08-04 17:18:37 +0700

    Actual PR action logic

--- pr/src/main.ts
+++ pr/src/main.ts
@@ -1,18 +1,44 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import {setFailed, getInput} from '@actions/core'
+import {env} from 'process'
+import * as fs from 'fs'
+import * as shell from 'shelljs'
+import {publishResults, UploadOptions} from 'nunit-result/src/publish-results'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const accessToken = getInput('access-token')
+    const imageName = getInput('image-name')
+    const dockerFilePath = getInput('docker-file-path')
+    const dockerTarget = getInput('docker-target')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const workspacePath = env['GITHUB_WORKSPACE']
+    const testResultsPath = `${workspacePath}/test-results`
+    fs.mkdirSync(testResultsPath)
 
-    core.setOutput('time', new Date().toTimeString())
+    let res = shell.exec(
+      `docker build -t ${imageName} --target ${dockerTarget} -f ${dockerFilePath} .`
+    )
+    if (res.code !== 0) {
+      setFailed(res.stderr)
+      return
+    }
+    res = shell.exec(
+      `docker run -v ${testResultsPath}:/app/test-results ${imageName}`
+    )
+    if (res.code !== 0) {
+      setFailed(res.stderr)
+      return
+    }
+
+    const options = new UploadOptions(
+      testResultsPath,
+      accessToken,
+      'Tests Report',
+      30
+    )
+    await publishResults(options)
   } catch (error) {
-    core.setFailed(error.message)
+    setFailed(error.message)
   }
 }
 
